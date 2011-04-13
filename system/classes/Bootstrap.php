50a56f774a9fa6378f851fb80d0bbed097c15275
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-13 17:28:28 +0300

    Direct loading of main language file.

--- system/classes/Bootstrap.php
+++ system/classes/Bootstrap.php
@@ -5,6 +5,11 @@ function __autoload($class)
     require_once SYSTEM . "classes/{$class}.php";
 }
 
+// load main configuration file
 Config::instance()->load('config');
-I18n::set_lang('el');
+
+// load main language file
+require_once APP . 'languages/' . I18n::instance()->current() . '.php';
+
+// start routing
 Route::set();
