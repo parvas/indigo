509b40768421e19492e9554a1b368cc9949ff9a3
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-18 21:32:24 +0300

    Edited default template.

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -3,15 +3,23 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<meta name="author" name="Indigo Labs">
+<meta name="author" name="Paraschos Vasiliadis">
 <meta name="generator" name="Indigo PHP Framework">
 <title><?php echo $title; ?></title>
 <meta name="description" content="<?php echo $description; ?>">
 <meta name="keywords" content="<?php echo $keywords; ?>">
-<link rel="stylesheet" type="text/css" media="all" href="<?=CSS?>styles.css">
+<link rel="stylesheet" href="<?=CSS?>styles.css">
 <?php echo $head; ?>
 </head>
 <body>
+    <header>
+        indigo
+    </header>    
+    <nav>
+        <ul>
+            <li><?php echo HTML::a(WEB . 'pages/add', _ADD_PAGE_); ?></li>
+        </ul>
+    </nav>
     <div id="container">
         <div id="langs">
             <?php echo I18n::instance()->locale_box(); ?>
@@ -19,8 +27,8 @@
         <?php echo "<h1>{$title}</h1>"; ?>
         <?php echo $body; ?>
     </div>
-    <div id="footer">
+    <footer>
     Powered by <a id="indigo" target="_blank" href="https://github.com/parvas/indigo">indigo</a>
-    </div>
+    </footer>
 </body>
 </html>
