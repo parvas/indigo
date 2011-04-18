55fe2dca3fbb24c49280be7de70ac2b8ed533521
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-18 22:38:12 +0300

    Added 404 error page.

---
+++ application/views/404.php
@@ -0,0 +1,29 @@
+<?php if (!defined('SYSTEM')) exit('No direct script access allowed'); ?>
+
+<style>
+    .e_404 {
+        margin: 20px;
+        border: 1px dotted #888;
+    }
+    
+    .heading {
+        font-size: 18px;
+        background-color: #911;
+        color: #fff;
+        padding: 10px 20px;
+    }
+    
+    .text {
+        font-size: 14px;
+        padding: 5px 20px;
+    }
+</style>
+
+<div class="e_404">
+    <div class="heading">
+        Error 404
+    </div>
+    <div class="text">
+        The page you requested could not be found 
+    </div>
+</div>
