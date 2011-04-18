e85c6765b45f03a492d9cd2849b244f9cb3297b0
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-18 21:30:23 +0300

    Added locales configuration file.

---
+++ application/config/locales.php
@@ -0,0 +1,14 @@
+<?php defined('SYSTEM') or exit('No direct script access allowed');
+
+$locales = array(
+    
+        'el' => array(
+            'language'  => 'ελληνικά',
+            'active'    => true
+        ),
+    
+        'en' => array(
+            'language'  => 'English',
+            'active'    => true
+        )
+    );
