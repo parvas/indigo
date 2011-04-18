c170866d44b2b8208adf48cd68c997c4eb495b9c
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-18 21:50:19 +0300

    Added standard exception views.

---
+++ system/views/error.php
@@ -0,0 +1,43 @@
+<html>
+<head></head>
+<body>
+<style>
+    .indigo_error {
+        margin: 20px;
+        font-size: 13px;
+        border: 1px dotted #888;
+    }
+    
+    .error_header {
+        background-color: #911;
+        color: #fff;
+        padding: 10px 20px;
+        font-size: 13px;
+    }
+    
+    .error_details, .trace {
+        background-color: #ddd;
+        padding: 5px 20px;
+        font-family: arial;
+    }
+    
+    .trace {
+        font-size: 12px;
+    }
+    
+    pre {
+        font-family: arial;
+    }
+</style>
+<div class="indigo_error">
+    <div class="error_header"><?php echo $type; ?> &bull; <?php echo $message; ?></div>
+    <div class="error_details">
+        <?php echo $file; ?> [<?php echo $line; ?>]
+    </div>
+    <div class="trace">
+        <?php echo $trace?>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ system/views/php_error.php
@@ -0,0 +1,26 @@
+<style>
+    .indigo_error {
+        margin: 20px;
+        font-size: 13px;
+        border: 1px dotted #888;
+    }
+    
+    .error_header {
+        background-color: #911;
+        color: #fff;
+        padding: 5px 20px;
+        font-size: 13px;
+    }
+    
+    .error_details {
+        background-color: #ddd;
+        padding: 5px 20px;
+    }
+</style>
+
+<div class="indigo_error">
+    <div class="error_header"><?php echo $type; ?> &bull; <?php echo $message; ?></div>
+    <div class="error_details">
+        <?php echo $file; ?> [<?php echo $line; ?>]
+    </div>
+</div>
