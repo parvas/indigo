d0dc16a9afe855bf511bf8c4b4a9703a9d755c56
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-21 17:58:58 +0300

    Some more language strings.

--- application/languages/en.php
+++ application/languages/en.php
@@ -4,6 +4,8 @@ define ('_REQUIRED_',       'This field is required');
 define ('_TITLE_',          'Title');
 define ('_SUMMARY_',        'Summary');
 define ('_CONTENT_',        'Content');
+define ('_KEYWORDS_',       'Keywords');
+define ('_DESCRIPTION_',    'Description');
 define ('_SUBMIT_',         'Submit');
 define ('_RESET_',          'Reset');
 define ('_ADD_PAGE_',       'Add Page');
