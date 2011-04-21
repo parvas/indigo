901825e800534164a3057c938eaa2a6a4294af55
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-04-21 18:50:14 +0300

    Added locale codes in locales configuration file.

--- application/config/locales.php
+++ application/config/locales.php
@@ -4,11 +4,13 @@ $locales = array(
     
         'el' => array(
             'language'  => 'ελληνικά',
+            'locale'    => 'el-GR',
             'active'    => true
         ),
     
         'en' => array(
             'language'  => 'English',
+            'locale'    => 'en-GB',
             'active'    => true
         )
     );
