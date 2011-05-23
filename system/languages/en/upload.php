4d5007158110d6ac0b87f70edf2b9138384d9264
Paraschos Vasiliadis <parvas.webdev@gmail.com>
2011-05-23 21:08:05 +0300

    Added upload validation messages.

---
+++ system/languages/en/upload.php
@@ -0,0 +1,9 @@
+<?php if (!defined('SYSTEM')) exit('No direct script access allowed');
+
+$lang = array(
+    'general_error'     => 'Error uploading file "%s"',
+    'invalid_filetype'  => 'Filetype of file "%s" is not allowed',
+    'invalid_filesize'  => 'File "%s" exceeds the maximum permitted size',
+    'invalid_width'     => 'File "%s" exceeds the maximum width',
+    'invalid_height'    => 'File "%s" exceeds the maximum height'
+);
